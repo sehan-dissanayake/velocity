c2741f42e8bb22642a5d7eacea04524f116b1ca5
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2025-03-17 01:18:41 +0530

    setup

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api', authRoutes);
+app.use('/api/auth', authRoutes);
 
 // Start the server
 app.listen(PORT, () => {
