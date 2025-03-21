d24706a74ddfbc30778e3b6f66f7b4a97825741a
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2025-03-22 00:27:40 +0530

    updated the gradle file to work with

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -8,9 +8,12 @@ plugins {
 android {
     namespace = "com.example.frontend"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973" // Set the required NDK version explicitly
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // Add this line to enable desugaring
+        isCoreLibraryDesugaringEnabled = true
+        
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -20,20 +23,37 @@ android {
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.frontend"
-        minSdk = flutter.minSdkVersion
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        
+        // Make sure minSdk is at least 21
+        minSdk = if (flutter.minSdkVersion < 21) 21 else flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        // Enable multidex support
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
+dependencies {
+    // Add this line for desugaring support
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
+    
+    // If you need multidex support, add this
+    implementation("androidx.multidex:multidex:2.0.1")
+}
 
 flutter {
     source = "../.."
