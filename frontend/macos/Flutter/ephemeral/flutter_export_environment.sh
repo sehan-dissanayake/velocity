59ec712161492800869eb0d48da4fa3ef5fa40c2
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2025-03-22 00:40:02 +0530

    my pc

--- frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gayathriharshila/Desktop/Db_project/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gayathriharshila/Desktop/Db_project/GitHub/velocity/frontend"
+export "FLUTTER_ROOT=/Users/ashidudissanayake/Dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ashidudissanayake/Dev/velocity/frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
